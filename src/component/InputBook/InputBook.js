1648fb5d9f3c1978327f77f8170f259deb8786af
gedewon <gedewon.hayle.9@gmail.com>
2022-06-10 21:51:10 +0300

    Add design layout

---
+++ src/component/InputBook/InputBook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InputBook() {
+  return (
+    <div>InputBook</div>
+  )
+}
+
+export default InputBook
---
+++ src/component/Layout/DefaultLayout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DefaultLayout() {
+  return (
+    <div>DefaultLayout</div>
+  )
+}
+
+export default DefaultLayout
