ccf56e82f4fc14b0b87227c569ca474798bc3706
gedewon <gedewon.hayle.9@gmail.com>
2022-06-15 02:32:56 +0300

    Category checkstatus

--- src/component/pages/Categories.js
+++ src/component/pages/Categories.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
 function Categories() {
+  const dispatch = useDispatch();
+
   return (
-    <button type="button">Check Status</button>
+    <button type="button" onClick={()=>dispatch(checkStatus())}>Check Status</button>
   );
 }
 
