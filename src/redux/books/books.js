f813a247be8671061ce598093788b4607f8082b6
gedewon <gedewon.hayle.9@gmail.com>
2022-06-15 01:15:37 +0300

    Complete books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,8 +2,15 @@ const ADD = 'BOOKSTORE/books/ADD';
 const REMOVE = 'BOOKSTORE/books/REMOVE';
 
 
-export default (state={},action)=>{
+export default (state=[],action)=>{
     switch(action.type){
+        case ADD:
+            return [
+                ...state,
+                action.payload
+            ]
+        case REMOVE:
+            return state.filter(book=>book.id !== action.payload)
         default:
             return state;
     }
@@ -14,7 +21,7 @@ export default (state={},action)=>{
 
 export const addBook=(payload)=>({type:ADD,payload});
 
-export const removeBook=(payload)=>({type:REMOVE,payload})
+export const removeBook=(id)=>({type:REMOVE,id})
 
 
 
