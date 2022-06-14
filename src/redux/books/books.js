96e863824afcbfe161b76a65f31b835f51d86cd8
gedewon <gedewon.hayle.9@gmail.com>
2022-06-15 01:03:31 +0300

    Add action creator's

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,12 +2,19 @@ const ADD = 'BOOKSTORE/books/ADD';
 const REMOVE = 'BOOKSTORE/books/REMOVE';
 
 
+export default (state={},action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+
+}
 
 
 
+export const addBook=(payload)=>({type:ADD,payload});
 
-
-
+export const removeBook=(payload)=>({type:REMOVE,payload})
 
 
 
