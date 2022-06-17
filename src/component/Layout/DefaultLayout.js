7971c443f100751b66a3ed59c9f1ad4a9e53170f
gedewon <gedewon.hayle.9@gmail.com>
2022-06-17 17:52:27 +0300

    Style Layout component

--- src/component/Layout/DefaultLayout.js
+++ src/component/Layout/DefaultLayout.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
+import  style from './DefaultLayout.module.css';
 function DefaultLayout({ children }) {
   return (
-    <section>
-
-      <header>
-        <nav>
-          <ul>
-            <li><Link to="/books">Books</Link></li>
-            <li><Link to="/categories">Categories</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        {children}
+    <section className={style.BookstoreCMSBG}>
+      <main className={style.panelbgmain}>
+          <header className={style.headerComponent} >
+            <span className={style.BookstoreCMS}>
+            Bookstore CMS
+            </span>
+            <nav>
+              <ul>
+                <li className={`${style.BOOKS} ${style.TextStyle3}`}>
+                  <Link to="/books">BOOKS</Link></li>
+                <li className={`${style.CATEGORIES} ${style.TextStyle3}`}>
+                  <Link to="/categories">CATEGORIES </Link></li>
+              </ul>
+            </nav> 
+          </header>
+          <section>
+            {children}
+          </section>
       </main>
     </section>
   );
